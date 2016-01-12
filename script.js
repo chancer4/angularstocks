71ad0152a12e8f8a89605c2c76f533a203259e37
Chance Rhodes <chance.rhodes5@gmail.com>
2016-01-12 15:07:33 -0500

    Made Typeahead function
    figured out how to data bind to one ng-model then change the ng-model
    to bind to userStocks model and complete the search

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope,
   var _selected;
 
   $scope.selected = undefined;
-  $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+  $scope.states = ['AAPL','GOOG','BAC','YHOO','HOG','GE','SPY','DIA','QQQ'];
   // Any function returning a promise object can be used to load values asynchronously
   $scope.getLocation = function(val) {
     return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
